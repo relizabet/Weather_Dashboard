6bac544329eb09824cf52da87858d1ee52b5133c
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-07-29 19:33:41 -0400

    working still

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,14 @@
-console.log("this");
+$(document).ready(function () {
+  const inputField = $("#city_state");
+  const searchButton = $(".btn-floating");
+  let changeCity = $(".add-city");
+
+  searchButton.click(function (event) {
+    event.preventDefault();
+    console.log(inputField.val());
+    function addCity() {
+      $(changeCity).append("<p>Test</p>");
+    }
+    addCity();
+  });
+});
