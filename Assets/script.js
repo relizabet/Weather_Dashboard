dd00d61e8eea085242ac1cfaf723dfc43ad96f59
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-08-01 07:27:41 -0400

    persistance proglem

--- Assets/script.js
+++ Assets/script.js
@@ -2,46 +2,46 @@ $(document).ready(function () {
   const inputField = $("#city_state");
   const searchButton = $(".btn-floating");
   let changeCity = $(".addCity");
+  let nameName = "city";
   let nameValue = 0;
 
   searchButton.click(function (event) {
     event.preventDefault();
     let cityInput = inputField.val();
+    nameValue = nameValue += 1;
+    let nameAll = nameName + nameValue;
+    let getNameAll = `button#${nameAll}`;
+
     function addCity() {
-      nameValue = nameValue += 1;
       $(changeCity).append(
         `<button class='col l8 center-align m8 s8 add-city z-depth-1 btn' name=${nameValue}>${cityInput}</button>`
       );
+      window.localStorage.setItem(nameAll, cityInput);
+      $(getNameAll).val(window.localStorage.getItem(cityInput));
     }
 
     addCity();
   });
+
+  // searchButton.click(function (event) {
+  //   event.preventDefault();
+  //   let cityInput = inputField.val();
+  //   function addCity() {
+  //     nameValue = nameValue += 1;
+  //     let nameAll = nameName + nameValue;
+  //     let getNameAll = `div#${nameAll}`;
+  //     console.log(nameAll);
+
+  //     $(changeCity).attr("name", nameAll);
+  //     window.localStorage.setItem(nameAll, cityInput);
+  //     $(getNameAll).val(window.localStorage.getItem(nameAll));
+  //     console.log($(getNameAll).val(window.localStorage.getItem(nameAll)));
+  //     console.log(localStorage);
+  //   }
+
+  //   addCity();
+  // });
 });
 
-{
-  /* <p class="col l9 center-align m9 s9 "></p> */
-}
-
-// searchButton.click(function (event) {
-//   event.preventDefault();
-//   let cityInput = inputField.val();
-//   function addCity() {
-//     $(changeCity).append(cityInput).addClass("add-city");
-//   }
-//   addCity();
-// });
-
-// $(document).ready(function () {
-//   const inputField = $("#city_state");
-//   const searchButton = $(".btn-floating");
-//   let changeCity = $(".add-city");
-
-//   searchButton.click(function (event) {
-//     event.preventDefault();
-//     console.log(inputField.val());
-//     function addCity() {
-//       $(changeCity).append("<p>Test</>");
-//     }
-//     addCity();
-//   });
-// });
+// address:
+// [] text overflow on search buttons
