560b49efe122468796ae2785cc79db97ecafd22d
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-08-06 20:53:37 -0400

    5 day

--- Assets/script.js
+++ Assets/script.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
   const uvIndex = $("p.uvi");
   const temperature = $("p.temp");
   const windSpeed = $("p.wind");
-  const CityNameDate = $("h4.city-name-date");
   let changeCity = $("div.addCity");
   let nameName = "city";
   let nameValue = 0;
@@ -13,20 +12,24 @@ $(document).ready(function () {
   const urlWeather = "https://api.openweathermap.org/data/2.5/weather?q=";
   const urlWeatherUV = "http://api.openweathermap.org/data/2.5/uvi?";
   const unitImperial = "&units=imperial";
+  let degree = String.fromCharCode(176);
+
+  // get date
+  $("h4.city-name-date").text(moment().format("MMM Do[,] YYYY"));
+  let dateIndex = 1;
+  while (dateIndex <= 5) {
+    $(`div.date-${dateIndex}`).text(
+      moment().add(`${dateIndex}`, "days").format("MMM Do[,] YYYY")
+    );
+    dateIndex++;
+  }
 
   // search button click, add button
   searchButton.click(function (event) {
     event.preventDefault();
-    // $("p.temp")[0].val("");
-    let clearOut = $("p.temp");
-    console.log(clearOut.val());
-    $("p.hum").val("");
-    $("p.wind").val("");
-    $("p.uvi").val("");
     $("p.do-hide").hide();
 
     let cityInput = inputField.val().trim();
-    console.log(cityInput);
     nameValue = nameValue += 1;
     let nameAll = nameName + nameValue;
     let getNameAll = `button#${nameAll}`;
@@ -43,63 +46,67 @@ $(document).ready(function () {
 
     addCity();
     getWeather(cityInput);
-    getWeatherExtended(cityInput);
   });
 
   // get today's UV index
   function getUV(lat, lon) {
     $.ajax({
-      url: `${urlWeatherUV}${apiKey}&lat=${lat}&lon=${lon}`,
+      url: `${urlWeatherUV}${apiKey}${lat}${lon}`,
       method: "GET",
     }).then(function (response) {
       let uvIndexVal = `UV Index: ${response.value}`;
-      console.log(response);
 
-      uvIndex.append(uvIndexVal);
+      uvIndex.text(uvIndexVal);
     });
   }
 
   // get today's weather
   function getWeather(cityInput) {
-    // change this
-    console.log(cityInput);
     let cityName = cityInput;
-    console.log(CityNameDate[0].innerHTML);
 
     $.ajax({
       url: `${urlWeather}${cityName}${unitImperial}${apiKey}`,
       method: "GET",
     }).then(function (response) {
-      console.log(response);
       // get temp
-      console.log(response);
-      let temperatureValue = `Temperature: ${response.main.temp}`;
-      let humidityValue = `Humidity: ${response.main.humidity}`;
-      let windSpeedValue = `Wind Speed: ${response.wind.speed}`;
+      let temperatureValue = `Temperature: ${response.main.temp}${degree} F`;
+      let humidityValue = `Humidity: ${response.main.humidity}%`;
+      let windSpeedValue = `Wind Speed: ${response.wind.speed} mph`;
 
-      temperature.append(temperatureValue);
-      humidity.append(humidityValue);
-      windSpeed.append(windSpeedValue);
+      temperature.text(temperatureValue);
+      humidity.text(humidityValue);
+      windSpeed.text(windSpeedValue);
 
-      let lon = response.coord.lon;
-      let lat = response.coord.lat;
+      let lon = `&lon=${response.coord.lon}`;
+      let lat = `&lat=${response.coord.lat}`;
 
       getUV(lat, lon);
+      getWeatherExtended(lat, lon);
     });
   }
 
-  function getWeatherExtended(cityInput) {
-    console.log(cityInput);
-    let cityName = cityInput;
-
+  function getWeatherExtended(lat, lon) {
     $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}${unitImperial}${apiKey}`,
+      url: `https://api.openweathermap.org/data/2.5/onecall?${unitImperial}${lat}${lon}${apiKey}`,
       method: "GET",
     }).then(function (response) {
-      console.log(response);
+      let index = 1;
+      while (index <= 5) {
+        // get weather icons
+        $(`img.icon-display${index}`).attr(
+          "src",
+          `http://openweathermap.org/img/wn/${response.daily[index].weather[0].icon}@2x.png`
+        );
+        // get temp
+        $(`p.temp-${index}`).text(
+          `Temp: ${response.daily[index].temp.day}${degree} F`
+        );
+        // get humidity
+        $(`p.hum-${index}`).text(
+          `Humidity: ${response.daily[index].humidity}%`
+        );
+        index++;
+      }
     });
   }
 });
-
-// address:
-// [] text overflow on search buttons
